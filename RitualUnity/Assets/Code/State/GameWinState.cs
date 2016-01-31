7f6ec71a4d14821bfc5b0452b27c9339872a3fc5
John Tuttle <jtuttle@craw.cc>
2016-01-31 00:42:22 -0500

    floaty monks

--- RitualUnity/Assets/Code/State/GameWinState.cs
+++ RitualUnity/Assets/Code/State/GameWinState.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameWinState : BaseGameEndState {
 	private AudioClip _winMusic;
 
+	private float FLY_SPEED = 0.03f;
+
 	public GameWinState()
 		: base(GameState.GameWin) {
 
@@ -19,6 +21,28 @@ public class GameWinState : BaseGameEndState {
 	public override void EnterState(FSMTransition transition) {
 		base.EnterState(transition);
 
+		Camera.main.GetComponent<FollowCamera>().enabled = false;
+
 		PlayEndMusic(_winMusic);
+	}
+
+	public override void ExitState(FSMTransition transition) {
+		Camera.main.GetComponent<FollowCamera>().enabled = true;
+
+		foreach(GameObject monk in GameData.Monks) {
+			Vector3 monkPos = monk.transform.position;
+			monk.transform.position = new Vector3(monkPos.x, 0, monkPos.z);
+		}
+
+		base.ExitState(transition);
+	}
+
+	public override void Update() {
+		base.Update();
+
+		foreach(GameObject monk in GameData.Monks) {
+			Vector3 monkPos = monk.transform.position;
+			monk.transform.position = new Vector3(monkPos.x, monkPos.y + FLY_SPEED, monkPos.z);
+		}
 	}
 }
