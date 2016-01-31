c355bea11c2e760a78c4118f7c18bfe711a59c12
John Tuttle <jtuttle@craw.cc>
2016-01-31 01:44:21 -0500

    save main scene

--- RitualUnity/Assets/Code/State/GamePlayState.cs
+++ RitualUnity/Assets/Code/State/GamePlayState.cs
@@ -101,7 +101,7 @@ public class GamePlayState : FSMState {
 	}
 
 	private bool HasCorrectPlacement() {
-		return false;
+		return true;
 
 		GameObject leftMonk = GameData.Monks[GameData.PlayerNote - 1];
 		GameObject rightMonk = GameData.Monks[GameData.PlayerNote];
